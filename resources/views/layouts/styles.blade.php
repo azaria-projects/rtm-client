17252cba6c52674a05be8e2f2e9ec35e14b34292
azaria-fairuz <fairuz.akbar.azaria@gmail.com>
2025-09-16 19:58:51 +0700

    20250916

--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -1,4 +1,9 @@
 @push('styles')
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inria+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@$ICONS_VERSION/dist/tabler-icons.min.css" />
+
     <link rel="stylesheet" href="{{ asset('themes/adminlte4/css/adminlte.min.css') }}">
     <link rel="stylesheet" href="{{ asset('dependancies/select2/css/select2.min.css') }}">
     <link rel="stylesheet" href="{{ asset('dependancies/datatable/css/datatables.min.css') }}">
@@ -17,8 +22,4 @@
         'resources/css/components/buttons.css',
         'resources/css/components/notification.css',
     ])
-
-    <link rel="stylesheet" href="{{ asset('icons/tabler/tabler-icons-filled.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('icons/tabler/tabler-icons-outline.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('icons/tabler/tabler-icons.min.css') }}">
 @endpush
